28dbc3212f85f4f65cf71602ca8301c231067058
Anas Hmamouch <anashmamouch90@gmail.com>
2015-08-23 20:52:01 +0100

    Adding the places part

--- app/src/main/java/com/example/anas/mymap/MainActivity.java
+++ app/src/main/java/com/example/anas/mymap/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.anas.mymap;
 
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,12 +20,27 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private GoogleMap map;
     private LocationManager locationManager;
     private Marker userMarker;
+    private Marker[] placeMarkers;
+    private final int MAX_PLACES = 20;
+    private MarkerOptions[] places;
+
 
     private int userIcon;
     private int foodIcon;
@@ -38,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
 
         map = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 
+        placeMarkers = new Marker[MAX_PLACES];
+        
+
         userIcon = R.drawable.user_place;
         foodIcon = R.drawable.marker_teal;
         shopIcon = R.drawable.marker_yellow;
@@ -45,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         otherIcon = R.drawable.marker_orange;
 
         updatePlaces();
+
     }
 
     //method to update user location
@@ -75,6 +96,71 @@ public class MainActivity extends AppCompatActivity {
 
         //Animate the camera to zoom to the users position
         map.animateCamera(CameraUpdateFactory.newLatLng(lastLatLng), 3000, null);
+
+        //Google places search query
+        String placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/" +
+                "json?location="+lat+","+lng+
+                "&radius=1000&sensor=true" +
+                "&types=food|bar|store|museum|art_gallery"+
+                "&key=your_key_here";
+
+        GetPlaces getPlaces = new GetPlaces();
+        getPlaces.execute(placesSearchStr);
+        Log.d("BENZINO", getPlaces.toString());
+
+    }
+
+    //Async task classes to fetch the places
+    private class GetPlaces extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... placesURL) {
+            //fetch and get places data
+            StringBuilder placesBuilder = new StringBuilder();
+
+            //process Search parameter string
+            for(String placesSearchURL:placesURL){
+                //execute search
+                HttpClient placesClient = new DefaultHttpClient();
+
+                try{
+                    //try to fetch the data
+                    HttpGet placesGet = new HttpGet(placesSearchURL);
+                    //retrieve Http response
+                    HttpResponse placesResponse = placesClient.execute(placesGet);
+                    //Check if we have a positive response HTTP:200 ok
+                    StatusLine status = placesResponse.getStatusLine();
+
+                    if(status.getStatusCode() == 200){
+                        //retrieve the actual content of the response data
+                        HttpEntity placesEntity = placesResponse.getEntity();
+                        InputStream placesContent = placesEntity.getContent();
+
+                        //create a reader for this stream
+                        InputStreamReader placesInput = new InputStreamReader(placesContent);
+
+                        BufferedReader placesReader = new BufferedReader(placesInput);
+
+                        String line = placesReader.readLine();
+
+                        while (line != null)
+                            placesBuilder.append(line);
+
+
+                    }
+                }catch (Exception e){
+
+                }
+            }
+            Log.d("BENZINO", "NNNNNVVVV " +  placesBuilder.toString());
+            return placesBuilder.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+        }
     }
 
     @Override
